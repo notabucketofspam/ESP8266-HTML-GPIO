e93d8cdccd49c62fc40f2da46da5f8f2b5f6f28f
notabucketofspam <not.a.bucket.of.spam@gmail.com>
2020-08-14 21:50:03 -0400

    Path again

--- mkspiffs_1M.sh
+++ mkspiffs_1M.sh
@@ -2,11 +2,11 @@
 echo "Default serial port..."
 read default_serial_port
 cd util
-cd mkspiffs; git submodule update --init; 
+cd mkspiffs; git submodule update --init --recursive; 
 make dist CPPFLAGS="-DSPIFFS_ALIGNED_OBJECT_INDEX_TABLES=1"
 ./mkspiffs -c ../../spiffs_image/ -b 4096 -p 256 -s 0x70000 ../spiffs.bin
 cd ..
-cd esptool
+cd esptool; git submodule update --init --recursive;
 pip install --user -e .
 ./esptool.py --port $default_serial_port write_flash 0x90000 ../spiffs.bin
 cd ..
